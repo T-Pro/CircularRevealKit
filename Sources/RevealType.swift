5304cd3ed0af38ee929b6d17d4ff8f091012f52f
Pedro Paulo Amorim <pepa.amorim@gmail.com>
2017-04-16 23:54:11 +0100

    Logo size updated

---
+++ Sources/RevealType.swift
@@ -0,0 +1,10 @@
+//
+// Created by Pedro Paulo de Amorim on 15/04/2017.
+//
+
+import Foundation
+
+public enum RevealType {
+  case reveal
+  case unreveal
+}
---
+++ Sources/UICircularViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+open class UICircularViewController: UIViewController {
+
+  override open func loadView() {
+    super.loadView()
+    setupBackButton()
+  }
+
+}

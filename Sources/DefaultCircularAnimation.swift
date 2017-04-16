ff2316a0d8e99dcdad365452a22c83557dbfdc7c
Pedro Paulo Amorim <pepa.amorim@gmail.com>
2017-04-16 23:54:25 +0100

    Logo size updated

--- Sources/DefaultCircularAnimation.swift
+++
@@ -1,8 +0,0 @@
-//
-// Created by Pedro Paulo de Amorim on 15/04/2017.
-//
-
-import Foundation
-
-internal let DEFAULT_CIRCULAR_ANIMATION_DURATION: TimeInterval = 0.33
-internal let ANIMATION_KEY_PATH: String = "path"
